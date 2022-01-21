bf3ccc5578e1adc86973b2a2ad8ecd940c022d18
swq <songwenqi@npnets.com>
2022-01-21 17:07:20 +0800

    分支测试1

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test1.java
@@ -0,0 +1,7 @@
+package com.xxl.job.executor.service.jobhandler.GitTest;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("gitTest1.....");
+    }
+}
