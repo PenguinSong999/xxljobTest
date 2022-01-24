5b879074ed109078f330c36dc80f666a20e07974
swq <995385367@qq.com>
2022-01-24 17:49:45 +0800

    Revert "版本回退测试"
    This reverts commit 6ddb085c

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test1.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test1.java
@@ -4,8 +4,4 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("gitTest1.....");
     }
-
-    public void test(){
-        System.out.println("版本回退测试》》》》");
-    }
 }
