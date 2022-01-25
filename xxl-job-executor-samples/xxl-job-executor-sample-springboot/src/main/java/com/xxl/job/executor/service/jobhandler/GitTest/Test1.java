b4045dcc9b6e2df69fa22b583f6b86c5e6e87b50
swq <995385367@qq.com>
2022-01-25 09:52:33 +0800

    revert测试版本回退2

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test1.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test1.java
@@ -9,9 +9,7 @@ public class Test1 {
         System.out.println("版本回退测试》》》》");
     }
 
-    public void test2(){
-        System.out.println("测试版本回退2222222222");
-    }
+
 
     public void test3(){
         System.out.println("测试版本回退33333333333");
