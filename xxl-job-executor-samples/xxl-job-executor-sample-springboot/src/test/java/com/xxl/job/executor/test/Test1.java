a660dfd33e1f0e3f0b2eff45283ab3c3fb6ba8e6
swq <songwenqi@npnets.com>
2022-02-23 10:35:58 +0800

    Test1分支合并冲突测试_master分支

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/test/java/com/xxl/job/executor/test/Test1.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/test/java/com/xxl/job/executor/test/Test1.java
@@ -174,8 +174,8 @@ public class Test1 {
     @Test
     public void test12(){
 //        List test = null;
-        ArrayList<Integer> test = new ArrayList<>();
-        Collections.sort(test, new Comparator<Object>() {
+        List<Integer> list = new ArrayList<>();
+        Collections.sort(list, new Comparator<Object>() {
             @Override
             public int compare(Object o1, Object o2) {
                 return (int) o1 - (int)o2;
