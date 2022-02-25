34707ec6d8e0335ae38fe58ec83d7f3af0a5fa8f
swq <songwenqi@npnets.com>
2022-02-25 13:46:31 +0800

    revert版本回退测试1

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test2.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test2.java
@@ -5,4 +5,9 @@ public class Test2 {
     public void test1(){
         System.out.println("测试Test22222222");
     }
+
+
+    public void tset2(){
+        System.out.println("revert版本回退测试");
+    }
 }
