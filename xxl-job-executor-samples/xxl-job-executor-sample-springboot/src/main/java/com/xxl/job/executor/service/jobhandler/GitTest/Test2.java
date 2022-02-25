924091cd12633c619f976a0f99e5e860f1f197fc
swq <songwenqi@npnets.com>
2022-02-25 13:48:33 +0800

    Revert "revert版本回退测试1"
    This reverts commit 34707ec6

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test2.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test2.java
@@ -5,9 +5,4 @@ public class Test2 {
     public void test1(){
         System.out.println("测试Test22222222");
     }
-
-
-    public void tset2(){
-        System.out.println("revert版本回退测试");
-    }
 }
