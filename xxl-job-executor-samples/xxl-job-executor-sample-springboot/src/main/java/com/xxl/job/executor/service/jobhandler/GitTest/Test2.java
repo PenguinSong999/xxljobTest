0e9e7aa37e22da832c718b6dff88734e7f6a6c2b
swq <songwenqi@npnets.com>
2022-07-01 11:38:25 +0800

    cherry-pick合并分支测试888888

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test2.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/GitTest/Test2.java
@@ -28,4 +28,8 @@ public class Test2 {
     public void test6(){
         System.out.println("cherry-pick合并分支测试666");
     }
+
+    public void test8(){
+        System.out.println("cherry-pick合并分支测试88888888");
+    }
 }
